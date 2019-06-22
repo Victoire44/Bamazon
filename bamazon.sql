d031fcf1b41078df185452467325ae891461676b
Victoire <victoirebaron@hotmail.fr>
2019-06-22 16:24:11 -0700

    creating Table products

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products(
+    item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(250) NOT NULL,
+    department_name VARCHAR(250) NOT NULL,
+    price INTEGER(30) NOT NULL,
+    stock_quantity INTEGER(250) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Uncharted 4","Video Games", 49.95, 150),
+("DOOM", "Video Games", 59.99, 150),
+("Crate of Spam", "Food and Drink", 24.5, 50),
+("Cool Shades", "Apparel", 75, 5),
+("Worn Denim jeans", "Apparel", 54.25, 35),
+("Survival Towel", "Necessities", 42.42, 42),
+("Bill and Ted's Axcellent Adventure", "Films", 15, 25),
+("Mad Max: Fury Road", "Films", 25.5, 57),
+("Monopoly", "Board Games", 30.5, 35),
+("Yahtzee", "Board Games", 19.95, 23),
+
